c27145b85bf6d98dbe567773be417a668ed71be6
anshula1610 <anshula16102005@gmail.com>
2025-05-02 13:29:33 +0530

    Added new files

--- p7.cpp
+++ p7.cpp
@@ -5,14 +5,14 @@ int main(){
         int value=i; 
         for(int j=0;j<=i;j++){
             cout<<value;
-            value--;
+            value++;
         }
         cout<<"\n";
     }
     for(int i=0;i<=3;i++){
         int value=i; 
         for(int j=0;j<=i;j++){
-            cout<<i-j;
+            cout<<i+j;
         }
         cout<<"\n";
     }
